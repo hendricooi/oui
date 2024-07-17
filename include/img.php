b951003f4e38e29e951955c9abd1955f508c998a
hendricooi <hendric2008@gmail.com>
2024-07-17 12:29:28 +0800

    change to xhr fetch data

--- include/img.php
+++ include/img.php
@@ -1,28 +1,32 @@
 <script>
-function EquipmentAvailability() {
-    const controller = new AbortController();
-    const timeoutId = setTimeout(() => {
-        controller.abort();
-    }, 2500);
-    fetch('../api/received_data.json')
-        .then(response => response.json())
-        .then(data => {
-            const itemExists = Object.keys(data).includes("<?php echo $key ?>");
-            const imgDiv = document.getElementById('img-<?php echo $key ?>');
-            if (itemExists) {
-                const colors = data["<?php echo $key ?>"].filter(item => item.Function === "SetEquipmentStatus").map(item => item.List1[0]);
-                imgDiv.src= colors[0].toUpperCase() === "BLUE" ? "../img/blue.png" : 
-                colors[0].toUpperCase() === "RED" ? "../img/circle-xxl.png" : 
-                colors[0].toUpperCase() === "GREEN" ? "../img/green.jpg" : "";
-            } else {
-                imgDiv.src = '../img/clock.png';
-            }
-        });
-}
+        function EquipmentAvailability() {
+            const xhr = new XMLHttpRequest();
+            xhr.open('GET', '../api/received_data.json', true);
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        const data = JSON.parse(xhr.responseText);
+                        const itemExists = Object.keys(data).includes("<?php echo $key ?>");
+                        const imgDiv = document.getElementById('img-<?php echo $key ?>');
+                        if (itemExists) {
+                            const colors = data["<?php echo $key ?>"].filter(item => item.Function === "SetEquipmentStatus").map(item => item.List1[0]);
+                            imgDiv.src = colors[0].toUpperCase() === "BLUE" ? "../img/blue.png" : 
+                                         colors[0].toUpperCase() === "RED" ? "../img/circle-xxl.png" : 
+                                         colors[0].toUpperCase() === "GREEN" ? "../img/green.jpg" : "";
+                        } else {
+                            imgDiv.src = '../img/clock.png';
+                        }
+                    } else {
+                        console.error('Error fetching data: ' + xhr.statusText);
+                    }
+                }
+            };
+            xhr.onerror = function() {
+                console.error('Network error');
+            };
+            xhr.send();
+        }
 
-EquipmentAvailability();
-
-setInterval(EquipmentAvailability, 3000);
-
-
-</script>
+        EquipmentAvailability();
+        setInterval(EquipmentAvailability, 5000);
+    </script>
